f4553c13c65854a9a72ba5aa41a9375cd8db9a1b
RomsenKabomsen <kondrjakow@posteo.de>
2021-09-09 15:36:26 +0200

    factorial test added

--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -1,2 +1,22 @@
-public class MathTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class AlarmTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1",
+            "1, 1",
+            "3, 6"}
+
+    )
+    public void checkAllowedNumberOfPersons(String threatLevel, int numberOfPeople, String expected){
+        //When
+        String actual = Alarm.switchAlarm(numberOfPeople, threatLevel);
+        //Then
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
